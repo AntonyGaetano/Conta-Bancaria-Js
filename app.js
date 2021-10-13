1bb1973b201c6c212e82f4cdd06270b60860320f
Antony Gaetano <86926752+AntonyGaetano@users.noreply.github.com>
2021-10-13 20:14:29 -0300

    Update app.js

--- app.js
+++ app.js
@@ -10,26 +10,25 @@ var valorAdicionado = 0
 function Adicionar(){
   let valor = prompt("Quanto irá adicionar?");
   valorAdicionado = Number(valorAdicionado) + Number(valor);
-
   document.querySelectorAll(".add p")[0].innerHTML = "R$" + valorAdicionado;
-
+  localStorage.setItem(valorAdicionado,"valorAdicionado");
   SaldoAtual();
 }
 
 
 function Retirar(){
   let valor2 = prompt("Quanto irá retirar?");
-  valorRetirado = Number(valorAdicionado) + Number(valor);
-  
+  valorRetirado = Number(valorAdicionado) + Number(valor2);
   document.querySelectorAll(".withraw p")[0].innerHTML="-R$" + valorRetirado;
-
+  localStorage.setItem(valorRetirado,"valorRetirado");
   SaldoAtual();
 }
 
 
 function SaldoAtual(){
   valor_atual = valorAdicionado - valorRetirado;
-
   document.querySelectorAll(".saldo_atual p")[0].innerHTML = "R$" + valor_atual
+  localStorage.setItem(valor_atual,"valorAtual");
 }
+
 
